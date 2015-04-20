62eb5cd05232ecd6e205a4eeda964d18c0250ce9
Tommy Chen <tommy351@gmail.com>
2015-04-21 01:21:37 +0800

    Add wercker.yml

--- model/main.go
+++ model/main.go
@@ -5,18 +5,27 @@ import (
 
 	"path/filepath"
 
+	"os"
+
 	"bitbucket.org/liamstask/goose/lib/goose"
 	"github.com/jinzhu/gorm"
 	"github.com/tommy351/app-studio-server/util"
 )
+
+const DB_DIR = "db"
 
 var db gorm.DB
 
 func init() {
 	var dbconf *goose.DBConf
 	var err error
+	env := os.Getenv("GO_ENV")
 
-	dbconf, err = goose.NewDBConf(filepath.Join(util.GetBaseDir(), "db"), "development", "")
+	if env == "" {
+		env = "development"
+	}
+
+	dbconf, err = goose.NewDBConf(filepath.Join(util.GetBaseDir(), DB_DIR), env, "")
 
 	if err != nil {
 		log.Fatal(err)
